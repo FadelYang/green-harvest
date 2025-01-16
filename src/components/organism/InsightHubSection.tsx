9221375a5c8c84b595f9c8e172bebb75020005f0
fadelanumah <fadelanumah@gmail.com>
2025-01-16 23:13:26 +0700

    feat(home): add scrollable container in insigt hub section

--- src/components/organism/InsightHubSection.tsx
+++ src/components/organism/InsightHubSection.tsx
@@ -5,7 +5,7 @@ import arrowRight from "/img/arrowRight.svg";
 const InsightHubSection = () => {
   return (
     <>
-      <div className="flex flex-col py-36 gap-9">
+      <div className="flex flex-col pt-36 gap-9">
         <div>
           <div className="px-24 flex justify-between items-center max-w-[1444px] mx-auto">
             <h1 className="text-slate-800 text-[40px] font-medium leading-[48px]">
@@ -23,7 +23,7 @@ const InsightHubSection = () => {
           </div>
         </div>
         {/* Article section */}
-        <div className="flex pl-24 gap-9 overflow-hidden max-w-full">
+        <div className="flex pl-24 gap-9 overflow-x-auto max-w-full no-scrollbar pb-36">
           <div className="flex gap-9">
             <ArticleCard />
             <ArticleCard />
