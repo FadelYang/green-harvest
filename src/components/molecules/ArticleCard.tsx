f9a1d1b294931cd5c2cb79d73a6a798e969a284e
fadelanumah <fadelanumah@gmail.com>
2025-01-17 23:28:54 +0700

    fix(home): fix image content in insight hub section

--- src/components/molecules/ArticleCard.tsx
+++ src/components/molecules/ArticleCard.tsx
@@ -1,40 +1,35 @@
-import articleImage1 from "/img/article-image-1.png";
+type ArticleCard = {
+  thumbnailImage: string;
+  insightCategory: string;
+  createdAt: string;
+  title: string;
+  description: string;
+};
 
-const ArticleCard = () => {
+const ArticleCard = (props: ArticleCard) => {
+  const { thumbnailImage, insightCategory, createdAt, title, description } =
+    props;
+
   return (
     <>
       <div className="flex flex-col w-[496px] gap-4 shadow-xl">
-        <div>
-          <img src={articleImage1} alt="" />
+        <div className=''>
+          <img src={thumbnailImage} alt="" className='max-w-[496px] min-h-[298px]'/>
         </div>
         <div className="flex flex-col gap-2 px-6 pb-6">
           <div className="flex justify-between text-gray-500 ">
-            <p className="font-semibold text-base leading-[26px]">Article</p>
-            <p className="font-normal leading-[34px]">10/11/2024</p>
+            <p className="font-semibold text-base leading-[26px]">{insightCategory}</p>
+            <p className="font-normal leading-[34px]">{createdAt}</p>
           </div>
           <div className="flex flex-col gap-2">
             <div className="max-h-[73px] overflow-hidden">
               <h1 className="text-slate-800 font-semibold text-2xl leading-[34px] tracking-[0.96px] line-clamp-2">
-                Maklon Skincare Tangerang, Solusi Praktis Membuat Produk
-                Skincare dan Kosmetik Berkualitas di Harvest Group
+                {title}
               </h1>
             </div>
             <div className="max-h-[112px] overflow-hidden">
               <p className="text-slate-500 font-normal line-clamp-4">
-                Industri skincare dan kosmetik di Indonesia terus tumbuh pesat.
-                Di tengah kebutuhan produk perawatan kulit yang semakin
-                meningkat, bisnis skincare menjadi salah satu peluang yang
-                sangat menjanjikan. Namun, bagi para pengusaha yang ingin
-                meluncurkan produk skincare atau kosmetik dengan merek sendiri,
-                tantangan seperti biaya produksi, riset, regulasi, dan
-                pengemasan bisa sangat menguras waktu dan biaya.
-                Industri skincare dan kosmetik di Indonesia terus tumbuh pesat.
-                Di tengah kebutuhan produk perawatan kulit yang semakin
-                meningkat, bisnis skincare menjadi salah satu peluang yang
-                sangat menjanjikan. Namun, bagi para pengusaha yang ingin
-                meluncurkan produk skincare atau kosmetik dengan merek sendiri,
-                tantangan seperti biaya produksi, riset, regulasi, dan
-                pengemasan bisa sangat menguras waktu dan biaya.
+                {description}
               </p>
             </div>
           </div>
--- src/components/organism/InsightHubSection.tsx
+++ src/components/organism/InsightHubSection.tsx
@@ -1,6 +1,38 @@
 import Button from "../atoms/Button";
 import ArticleCard from "../molecules/ArticleCard";
 import arrowRight from "/img/arrowRight.svg";
+import articleImage1 from "/img/article-image-1.png";
+import articleImage2 from "/img/article-image-2.png";
+import articleImage3 from "/img/article-image-3.png";
+
+const insightHubArticleData = [
+  {
+    imageThumbnail: articleImage1,
+    insightCategory: "ARTICLE",
+    title:
+      "Maklon Skincare Tangerang, Solusi Praktis Membuat Produk Skincare dan Kosmetik Berkualitas di Harvest Group",
+    description:
+      "Industri skincare dan kosmetik di Indonesia terus tumbuh pesat. Di tengah kebutuhan produk perawatan kulit yang semakin meningkat, bisnis skincare menjadi salah satu peluang yang sangat menjanjikan. Namun, bagi para pengusaha yang ingin meluncurkan produk skincare atau kosmetik dengan merek sendiri, tantangan seperti biaya produksi, riset, regulasi, dan pengemasan bisa sangat menguras waktu dan biaya.",
+    createdAt: "10/11/2024",
+  },
+  {
+    imageThumbnail: articleImage2,
+    insightCategory: "ARTICLE",
+    title: "Mengapa Memilih Pabrik Maklon di Tangerang?",
+    description:
+      "Dalam dunia bisnis produk kecantikan dan kesehatan, banyak orang yang memiliki ide brilian tetapi terkendala oleh keterbatasan fasilitas dan biaya produksi. Di sinilah pabrik maklon memainkan peran penting, terutama bagi para pemilik merek yang ingin menciptakan produk berkualitas tanpa harus repot dengan urusan produksi. Harvest Group di Tangerang hadir sebagai solusi maklon terlengkap, dengan layanan yang mencakup produk skincare, kosmetik, minuman serbuk fiber, kolagen, hingga whey protein.",
+    createdAt: "09/11/2024",
+  },
+  {
+    imageThumbnail: articleImage3,
+    insightCategory: "ARTICLE",
+    title:
+      "Mengapa Jasa Maklon Menjadi Solusi Terbaik untuk Bisnis Produk Kesehatan dan Kecantikan Anda?",
+    description:
+      "Dalam dunia bisnis produk kecantikan dan kesehatan, banyak orang yang memiliki ide brilian tetapi terkendala oleh keterbatasan fasilitas dan biaya produksi. Di sinilah pabrik maklon memainkan peran penting, terutama bagi para pemilik merek yang ingin menciptakan produk berkualitas tanpa harus repot dengan urusan produksi. Harvest Group di Tangerang hadir sebagai solusi maklon terlengkap, dengan layanan yang mencakup produk skincare, kosmetik, minuman serbuk fiber, kolagen, hingga whey protein.",
+    createdAt: "09/11/2024",
+  },
+];
 
 const InsightHubSection = () => {
   return (
@@ -25,11 +57,16 @@ const InsightHubSection = () => {
         {/* Article section */}
         <div className="flex pl-24 gap-9 overflow-x-auto max-w-full no-scrollbar pb-36">
           <div className="flex gap-9">
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
+            {insightHubArticleData.map((item, index) => (
+              <ArticleCard
+                key={index}
+                thumbnailImage={item.imageThumbnail}
+                createdAt={item.createdAt}
+                title={item.title}
+                description={item.description}
+                insightCategory={item.insightCategory}
+              />
+            ))}
           </div>
         </div>
       </div>
