faeff634cf2c28ca85cca9e84363ce3052b850fe
fadelanumah <fadelanumah@gmail.com>
2025-01-17 11:55:49 +0700

    feat(service): add service page

---
+++ src/components/organism/ServiceHeroSection.tsx
@@ -0,0 +1,35 @@
+import serviceHeroImage1 from '/img/service-hero-1.png' 
+import serviceHeroImage2 from '/img/service-hero-2.png' 
+import serviceHeroImage3 from '/img/service-hero-3.png' 
+import serviceHeroImage4 from '/img/service-hero-4.png' 
+import Button from '../atoms/Button';
+
+const ServiceHeroSection = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-8 py-36 mx-auto max-w-[1444px]">
+        <div className="px-24 flex gap-[200px]">
+          <h1 className="text-slate-800 text-[44px] leading-[54px] font-medium">
+            Your Truested Partner in High Quality Maklon Services.
+          </h1>
+          <h2 className="flex items-end text-slate-500 leading-7 text-[18px] font-normal">
+            Launch your brand with confidence. From formulation to packaging, we
+            provide end-to-end Maklon services solutions that meet the highest
+            industry standards.
+          </h2>
+        </div>
+        <div className="px-24 z-10">
+          <Button variant="primary-button">Get Free Consultation</Button>
+        </div>
+        <div className="flex gap-3 -mt-16">
+          <img src={serviceHeroImage1} alt="" />
+          <img src={serviceHeroImage2} alt="" />
+          <img src={serviceHeroImage3} alt="" />
+          <img src={serviceHeroImage4} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ServiceHeroSection;
--- src/components/pages/Service.tsx
+++ src/components/pages/Service.tsx
@@ -1,11 +1,19 @@
-import MainTemplate from '../templates/MainTemplate';
+import LastBannerSection from '../organism/LastBannerSection';
+import ProductAvailableSection from '../organism/ProductAvailableSection';
+import ProductSection from '../organism/ProductSection';
+import ServiceHeroSection from '../organism/ServiceHeroSection';
+import MainTemplate from "../templates/MainTemplate";
+
 
 const Service = () => {
   return (
     <MainTemplate>
-      <div className='flex items-center justify-center'>This page is under construction</div>
+      <ServiceHeroSection />
+      <ProductSection />
+      <ProductAvailableSection />
+      <LastBannerSection />
     </MainTemplate>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
