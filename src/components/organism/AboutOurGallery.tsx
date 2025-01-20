469461fcf5f039a68c60fe73d0e4514a8db14528
fadelanumah <fadelanumah@gmail.com>
2025-01-20 17:25:29 +0700

    feat(about): add latest banner setion

--- src/components/organism/AboutOurGallery.tsx
+++ src/components/organism/AboutOurGallery.tsx
@@ -6,7 +6,7 @@ import ourGalleryImage4 from "/img/our-gallery-image-4.png";
 const AboutOurGallery = () => {
   return (
     <>
-      <div className="max-w-[1444px] pb-36 px-24 mx-auto flex justify-between">
+      <div className="max-w-[1444px] px-24 mx-auto flex justify-between">
         <h1 className="text-slate-800 text-2xl font-medium min-w-[130px]">Our Gallery</h1>
         <div className="flex gap-6 justify-end flex-wrap">
           <img src={ourGalleryImage1} alt="" />
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,6 +1,7 @@
 import AboutHeroSection from '../organism/AboutHeroSection';
 import AboutOurGallery from '../organism/AboutOurGallery';
 import AboutOurStorySection from '../organism/AboutOurStorySection';
+import LastBannerSection from '../organism/LastBannerSection';
 import MainTemplate from '../templates/MainTemplate';
 
 const About = () => {
@@ -9,6 +10,7 @@ const About = () => {
       <AboutHeroSection />
       <AboutOurStorySection />
       <AboutOurGallery />
+      <LastBannerSection />
     </MainTemplate>
   )
 }
