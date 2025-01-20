664644138d3bea827c0da37df6a557c1abe92e21
fadelanumah <fadelanumah@gmail.com>
2025-01-20 17:24:20 +0700

    feat(about): add our gallery section

---
+++ src/components/organism/AboutOurGallery.tsx
@@ -0,0 +1,22 @@
+import ourGalleryImage1 from "/img/our-gallery-image-1.png";
+import ourGalleryImage2 from "/img/our-gallery-image-2.png";
+import ourGalleryImage3 from "/img/our-gallery-image-3.png";
+import ourGalleryImage4 from "/img/our-gallery-image-4.png";
+
+const AboutOurGallery = () => {
+  return (
+    <>
+      <div className="max-w-[1444px] pb-36 px-24 mx-auto flex justify-between">
+        <h1 className="text-slate-800 text-2xl font-medium min-w-[130px]">Our Gallery</h1>
+        <div className="flex gap-6 justify-end flex-wrap">
+          <img src={ourGalleryImage1} alt="" />
+          <img src={ourGalleryImage2} alt="" />
+          <img src={ourGalleryImage3} alt="" />
+          <img src={ourGalleryImage4} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutOurGallery;
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,4 +1,5 @@
 import AboutHeroSection from '../organism/AboutHeroSection';
+import AboutOurGallery from '../organism/AboutOurGallery';
 import AboutOurStorySection from '../organism/AboutOurStorySection';
 import MainTemplate from '../templates/MainTemplate';
 
@@ -7,6 +8,7 @@ const About = () => {
     <MainTemplate>
       <AboutHeroSection />
       <AboutOurStorySection />
+      <AboutOurGallery />
     </MainTemplate>
   )
 }
