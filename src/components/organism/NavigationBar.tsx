3714ebda57467f29318cfc93c29a60896681d04b
fadelanumah <fadelanumah@gmail.com>
2025-01-21 10:25:54 +0700

    feat: add button to handle language changes

--- src/components/organism/NavigationBar.tsx
+++ src/components/organism/NavigationBar.tsx
@@ -1,10 +1,18 @@
 import navBarLogo from "/img/navbar-logo.png";
 import englishFlag from "/img/english-flag.svg";
+import indonesiaFlag from "/img/indonesia-flag.svg";
 import Button from "../atoms/Button";
 import { Link } from "react-router-dom";
+import { TFunction } from 'i18next';
 
-const NavigationBar = (props: any) => {
-  const { t } = props;
+type NavigationBarProps = {
+  t: TFunction<"translation", undefined>;
+  handleChangeLanguage: () => void;
+  currentLanguage: string;
+};
+
+const NavigationBar = (props: NavigationBarProps) => {
+  const { t, handleChangeLanguage, currentLanguage } = props;
 
   return (
     <div className="px-24 py-6 sticky top-0 overflow-hidden bg-white z-50">
@@ -16,10 +24,19 @@ const NavigationBar = (props: any) => {
               <img src={navBarLogo} alt="" className="h-[54px] w-[72px]" />
             </Link>
           </div>
-          <div className="flex gap-2 py-2 px-4 border border-slate-200 rounded-xl">
-            <img src={englishFlag} alt="" />
-            <p className="font-bold">EN</p>
-          </div>
+          <button onClick={() => handleChangeLanguage()}>
+            {currentLanguage === "en" ? (
+              <div className="flex gap-2 py-2 px-4 border border-slate-200 rounded-xl items-center">
+                <img src={englishFlag} alt="" className='max-w-4 max-h-3 shadow'/>
+                <p className="font-bold">EN</p>
+              </div>
+            ) : (
+              <div className="flex gap-2 py-2 px-4 border border-slate-200 rounded-xl items-center">
+              <img src={indonesiaFlag} alt="" className='max-w-4 max-h-3 shadow'/>
+              <p className="font-bold">ID</p>
+            </div>
+            )}
+          </button>
         </div>
 
         {/* Left Content */}
--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -1,23 +1,38 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { useTranslation } from "react-i18next";
 import NavigationBar from "../organism/NavigationBar";
 import Footer from "../organism/Footer";
 
 const MainTemplate = (props: PropsWithChildren) => {
   const { children } = props;
-  const { t } = useTranslation();
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(language);
+
+  const handleChangeLanguage = () => {
+    const newLanguage = currentLanguage === "en" ? "id" : "en";
+    console.log({ newLanguage });
+    setCurrentLanguage(newLanguage);
+    changeLanguage(newLanguage);
+  };
 
   return (
     // temporary like this
     <div className="flex flex-col min-h-screen">
       {/* Navigation Bar */}
-      <NavigationBar t={t}/>
+      <NavigationBar
+        t={t}
+        handleChangeLanguage={handleChangeLanguage}
+        currentLanguage={currentLanguage}
+      />
 
       {/* Main Content */}
       <main className="flex-grow">{children}</main>
 
       {/* Footer */}
-      <Footer t={t}/>
+      <Footer t={t} />
     </div>
   );
 };
