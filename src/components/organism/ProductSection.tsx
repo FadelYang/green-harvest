7b021913e88f20e4b338ce02c698d1e61339bf9c
fadelanumah <fadelanumah@gmail.com>
2025-01-21 14:35:27 +0700

    feat(home): add translation in product section

--- src/components/organism/ProductSection.tsx
+++ src/components/organism/ProductSection.tsx
@@ -9,47 +9,48 @@ import product3Open from "/img/product-3-open.png";
 import product3Close from "/img/product-3-close.png";
 import Button from "../atoms/Button";
 import { useNavigate } from "react-router-dom";
+import { useTranslationContext } from "../../context/TranslationContext";
 
 const ProductSection = () => {
   const [activeProduct, setActiveProduct] = useState<number>(1);
   const navigate = useNavigate();
+  const { t } = useTranslationContext();
 
   const products = [
     {
       id: 1,
       open: product1Open,
       close: product1Close,
-      closeText: "Herbal Maklon / Capsule",
+      closeText: t("home.product.1.closeText"),
       openText: {
-        title: "Herbal Maklon / Capsule",
-        description:
-          "Herbal Maklon with efficacy & packaging according to your needs. In the form of capsules, caplets, propolis, honey etc.",
-        buttonText: "See product that can be maklon",
-        buttonHref: "herbal",
+        title: t("home.product.1.openText.title"),
+        description: t("home.product.1.openText.description"),
+        buttonText: t("home.product.1.openText.buttonText"),
+        buttonHref: t("home.product.1.openText.buttonHref"),
       },
     },
     {
       id: 2,
       open: product2Open,
       close: product2Close,
+      closeText: t("home.product.2.closeText"),
       openText: {
-        title: "Health Drink Maklon",
-        description:
-          "Health & Beauty Drink Maklon in powder form such as diet drinks, Fiber, Collagen, Meal Replacement etc.",
-        buttonText: "See product that can be maklon",
-        buttonHref: "drink",
+        title: t("home.product.2.openText.title"),
+        description: t("home.product.2.openText.description"),
+        buttonText: t("home.product.2.openText.buttonText"),
+        buttonHref: t("home.product.2.openText.buttonHref"),
       },
     },
     {
       id: 3,
       open: product3Open,
       close: product3Close,
+      closeText: t("home.product.3.closeText"),
       openText: {
-        title: "Cosmetic Manufacturing",
-        description:
-          "Maklon Cosmetics & Facial Skincare, Hair Care & Body Skincare that is legal & according to your request.",
-        buttonText: "See product that can be maklon",
-        buttonHref: "cosmetic",
+        title: t("home.product.3.openText.title"),
+        description: t("home.product.3.openText.description"),
+        buttonText: t("home.product.3.openText.buttonText"),
+        buttonHref: t("home.product.3.openText.buttonHref"),
       },
     },
   ];
@@ -58,7 +59,7 @@ const ProductSection = () => {
     <>
       <div className="flex flex-col gap-10 py-36 px-24 text-center max-w-[1444px] mx-auto">
         <h1 className="text-[40px] font-medium leading-[50px] text-slate-800">
-          Greenlife Harvest Group Product Maklon Category
+          {t('home.product.header')}
         </h1>
         {/* product image container */}
         <div className="flex justify-center gap-0">
