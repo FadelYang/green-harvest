702e693f406c58091948645c39c0efff4ba0e317
fadelanumah <fadelanumah@gmail.com>
2025-01-21 15:14:58 +0700

    feat(home): add translation in video section

--- src/components/organism/VideoSection.tsx
+++ src/components/organism/VideoSection.tsx
@@ -1,31 +1,33 @@
+import { Trans } from 'react-i18next';
+import { useTranslationContext } from "../../context/TranslationContext";
 import Button from "../atoms/Button";
 import arrowRight from "/img/arrowRight.svg";
 import video from "/video/video.mp4";
 
 const VideoSection = () => {
+  const { t } = useTranslationContext();
+
   return (
     <>
       <div className="bg-[#EBFFF3]">
         <div className="flex flex-col gap-10 pl-24 py-36 max-w-[1444px] mx-auto">
           <div className="flex justify-between pr-24">
             <p className="text-[40px] font-medium leading-[50px] w-[611px]">
-              We are committed to being the best partner for your brand's
-              growth.
+              {t("home.video.header")}
             </p>
             <div className="w-[530px] flex flex-col gap-4">
               <p className="text-slate-800 font-normal">
-                <span className="font-bold">Greenlife Harvest Group</span> is a
-                leading private label manufacturer specializing in herbal
-                products, supplements, and cosmetics since 2004. Trusted by
-                hundreds of brands, our products reach over 50 countries
-                worldwide.
+                <Trans
+                  i18nKey={"home.video.subHeader"}
+                  components={{ bold: <span className="font-bold" /> }}
+                />
               </p>
               <div className="flex justify-start self-start">
                 <Button
                   paddingSize="px-6 py-4"
                   className="flex gap-3 primary-text font-medium text-base border-b border-[#015F26]"
                 >
-                  Explore Our Profile <img src={arrowRight} alt="" />
+                  {t('home.video.exploreOurProfileButton')} <img src={arrowRight} alt="" />
                 </Button>
               </div>
             </div>
