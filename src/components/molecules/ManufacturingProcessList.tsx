641a0bab0d8e5f32bea19a38b1bbcca25266bc52
fadelanumah <fadelanumah@gmail.com>
2025-01-21 16:52:34 +0700

    feat(home): add translation in manufacturing process section

--- src/components/molecules/ManufacturingProcessList.tsx
+++ src/components/molecules/ManufacturingProcessList.tsx
@@ -10,75 +10,73 @@ import manufacturingProcessImage8 from "/img/manufacturing-process-8.png";
 import manufacturingProcessImage9 from "/img/manufacturing-process-9.png";
 
 import ManufacturingProcessCard from "../atoms/ManufacturingProcessCard";
-
-const manufacturingProcesses = [
-  {
-    id: 1,
-    title: "Product Concept",
-    description:
-      "Consult with us about the product concept you want to create according to your needs.",
-    imageThumbnail: manufacturingProcessImage1
-  },
-  {
-    id: 2,
-    title: "Determining Formula",
-    description:
-      "Discuss your desired product formula and raw materials with our R&D expert team.",
-    imageThumbnail: manufacturingProcessImage2
-  },
-  {
-    id: 3,
-    title: "Sample Preperation",
-    description:
-    "We produce product samples according to the best concept & formulation that you want.",
-    imageThumbnail: manufacturingProcessImage3
-  },
-  {
-    id: 4,
-    title: "Deal Price",
-    description:
-      "A cooperation contract agreement for product manufacturing to be made and distributed in accordance with adequate procedures.",
-      imageThumbnail: manufacturingProcessImage4
-    },
-  {
-    id: 5,
-    title: "Choosing Packaging",
-    description:
-    "Discuss packaging design with our best designer team to support your brand & customer appeal.",
-    imageThumbnail: manufacturingProcessImage5
-  },
-  {
-    id: 6,
-    title: "Stability & Compatibility Test",
-    description:
-    "We perform a series of tests to ensure the formula will not change when filled into your product packaging.",
-    imageThumbnail: manufacturingProcessImage6
-  },
-  {
-    id: 7,
-    title: "Product License Registration",
-    description:
-    "Our team will help you process BPOM, HKI, and HALAL permits for your product until it's market-ready!",
-    imageThumbnail: manufacturingProcessImage7
-  },
-  {
-    id: 8,
-    title: "Production & Manufacture",
-    description:
-    "Choose us as your brand partner. We create your products with national and international standards.",
-    imageThumbnail: manufacturingProcessImage8
-  },
-  {
-    id: 9,
-    title: "Product Shipping",
-    description:
-    "We will send your products ready for distribution & marketing on time.",
-    imageThumbnail: manufacturingProcessImage9
-  },
-];
+import { useTranslationContext } from "../../context/TranslationContext";
 
 const ManufacturingProcessList = () => {
   const [openDetail, setOpenDetail] = useState<number | null>(1);
+  const { t } = useTranslationContext();
+
+  console.log({
+    "tes": t("home.manufacturingProcess.1.title")
+  });
+  
+
+  const manufacturingProcesses = [
+    {
+      id: 1,
+      title: t("home.manufacturingProcess.1.title"),
+      description:t("home.manufacturingProcess.1.description"),
+      imageThumbnail: manufacturingProcessImage1,
+    },
+    {
+      id: 2,
+      title: t("home.manufacturingProcess.2.title"),
+      description:t("home.manufacturingProcess.2.description"),
+      imageThumbnail: manufacturingProcessImage2,
+    },
+    {
+      id: 3,
+      title: t("home.manufacturingProcess.3.title"),
+      description:t("home.manufacturingProcess.3.description"),
+      imageThumbnail: manufacturingProcessImage3,
+    },
+    {
+      id: 4,
+      title: t("home.manufacturingProcess.4.title"),
+      description:t("home.manufacturingProcess.4.description"),
+      imageThumbnail: manufacturingProcessImage4,
+    },
+    {
+      id: 5,
+      title: t("home.manufacturingProcess.5.title"),
+      description:t("home.manufacturingProcess.5.description"),
+      imageThumbnail: manufacturingProcessImage5,
+    },
+    {
+      id: 6,
+      title: t("home.manufacturingProcess.1.title"),
+      description:t("home.manufacturingProcess.1.description"),
+      imageThumbnail: manufacturingProcessImage6,
+    },
+    {
+      id: 7,
+      title: t("home.manufacturingProcess.7.title"),
+      description:t("home.manufacturingProcess.7.description"),
+      imageThumbnail: manufacturingProcessImage7,
+    },
+    {
+      id: 8,
+      title: t("home.manufacturingProcess.8.title"),
+      description:t("home.manufacturingProcess.8.description"),
+      imageThumbnail: manufacturingProcessImage8,
+    },
+    {
+      id: 9,
+      title: t("home.manufacturingProcess.9.title"),
+      description:t("home.manufacturingProcess.9.description"),
+      imageThumbnail: manufacturingProcessImage9,
+    },
+  ];
 
   const toggleDetail = (id: number | null) => {
     setOpenDetail(openDetail === id ? null : id); // Close if open, open if closed
@@ -86,7 +84,7 @@ const ManufacturingProcessList = () => {
 
   return (
     <>
-      <div className='flex'>
+      <div className="flex">
         {manufacturingProcesses.map((item) => (
           <ManufacturingProcessCard
             key={item.id}
--- src/components/organism/ManufacturingProcessSection.tsx
+++ src/components/organism/ManufacturingProcessSection.tsx
@@ -1,20 +1,29 @@
-import Button from '../atoms/Button';
-import ManufacturingProcessList from '../molecules/ManufacturingProcessList';
+import { useTranslationContext } from "../../context/TranslationContext";
+import Button from "../atoms/Button";
+import ManufacturingProcessList from "../molecules/ManufacturingProcessList";
 
 const ManufacturingProcessSection = () => {
+  const { t } = useTranslationContext();
+
   return (
     <>
       <div className="flex flex-col gap-10 py-36 px-24 max-w-[1444px] mx-auto">
-        <div className='flex justify-between items-center'>
-          <div className='flex flex-col gap-2'>
-            <p className='text-slate-500 text-base font-semibold leading-[26px] uppercase'>PROCESS</p>
-            <p className='text-slate-800 text-[40px] font-medium leading-[50px]'>Our Manufacturing Process</p>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <p className="text-slate-500 text-base font-semibold leading-[26px] uppercase">
+              {t("home.manufacturingProcess.header")}
+            </p>
+            <p className="text-slate-800 text-[40px] font-medium leading-[50px]">
+              {t("home.manufacturingProcess.subHeader")}
+            </p>
           </div>
           <div>
-            <Button variant='primary-button'>Consult Now!</Button>
+            <Button variant="primary-button">
+              {t("home.hero.consultNowButton")}
+            </Button>
           </div>
         </div>
-        <div className=''>
+        <div className="">
           <ManufacturingProcessList />
         </div>
       </div>
