d24fa0bc95fedbfa31c939e0c3fb270bb956cd91
fadelanumah <fadelanumah@gmail.com>
2025-02-05 14:18:02 +0700

    feat(service): add responsive

--- src/components/organism/ServiceHeroSection.tsx
+++ src/components/organism/ServiceHeroSection.tsx
@@ -10,23 +10,23 @@ const ServiceHeroSection = (props: TranslationProps) => {
 
   return (
     <>
-      <div className="flex flex-col gap-8 py-36 mx-auto max-w-[1444px]">
-        <div className="px-24 flex gap-[200px]">
-          <h1 className="text-slate-800 text-[44px] leading-[54px] font-medium min-w-[520px]">
+      <div className="flex flex-col gap-8 xl:py-36 pt-10 mx-auto max-w-[1444px]">
+        <div className="xl:px-24 px-6 flex xl:flex-row flex-col xl:gap-[200px] gap-6">
+          <h1 className="text-slate-800 xl:text-[44px] text-[32px] xl:leading-[54px] leading-[42px] tracking-[-1.28px] font-medium xl:min-w-[520px]">
             {t("service.hero.header")}
           </h1>
-          <h2 className="flex items-end text-slate-500 leading-7 text-[18px] font-normal">
+          <h2 className="flex items-end text-slate-500 xl:text-[18px] text-[16px] leading-7 font-normal">
             {t("service.hero.subHeader")}
           </h2>
         </div>
-        <div className="px-24 z-10">
+        <div className="px-24 z-10 xl:block hidden">
           <Button variant="primary-button"> {t("service.hero.getFreeConsultationButton")}</Button>
         </div>
-        <div className="flex gap-3 -mt-16 overflow-hidden">
-          <img src={serviceHeroImage1} alt="" />
-          <img src={serviceHeroImage2} alt="" />
-          <img src={serviceHeroImage3} alt="" />
-          <img src={serviceHeroImage4} alt="" />
+        <div className="flex md:gap-3 gap-5 xl:-mt-16 overflow-hidden">
+          <img src={serviceHeroImage1} alt="" className='md:w-full w-[255px] md:ml:0 -ml-14'/>
+          <img src={serviceHeroImage2} alt="" className='md:w-full w-[227px]'/>
+          <img src={serviceHeroImage3} alt="" className='xl:block hidden'/>
+          <img src={serviceHeroImage4} alt="" className='xl:block hidden'/>
         </div>
       </div>
     </>
