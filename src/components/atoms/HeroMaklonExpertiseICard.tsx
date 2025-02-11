9ed7742586d089936a953025ff4ce3e345ad3193
fadelanumah <fadelanumah@gmail.com>
2025-02-11 13:27:28 +0700

    fix(home): fix maklon expertise card not center in mobile screen

--- src/components/atoms/HeroMaklonExpertiseICard.tsx
+++ src/components/atoms/HeroMaklonExpertiseICard.tsx
@@ -9,11 +9,11 @@ const HeroMaklonExpertiseItem = (props: HeroMaklonExpertiseItemProps) => {
 
   return (
     <>
-      <div className="max-w-[286px] min-h-[264px] p-4 flex flex-col gap-6 bg-slate-50">
-        <div className='md:block flex md:justify-start justify-center'>
+      <div className="max-w-[286px] min-h-[264px] p-4 flex flex-col gap-6 bg-slate-50 md:mx-0 mx-auto">
+        <div className='md:block flex xl:justify-start justify-center'>
           <img src={imagePath} alt="" />
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 md:text-start text-center">
           <p className="text-slate-800 text-xl font-semibold leading-[26px]">
             {title}
           </p>
